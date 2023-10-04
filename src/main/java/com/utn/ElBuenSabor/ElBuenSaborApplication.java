19bce30ba91778c7d6dfd9b62398884a8480d5a4
Janaina Bacin <114409955+janaBR30@users.noreply.github.com>
2023-10-04 17:49:49 -0300

    Proyect created

---
+++ src/main/java/com/utn/ElBuenSabor/ElBuenSaborApplication.java
@@ -0,0 +1,13 @@
+package com.utn.ElBuenSabor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElBuenSaborApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElBuenSaborApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utn/ElBuenSabor/ElBuenSaborApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.ElBuenSabor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElBuenSaborApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
